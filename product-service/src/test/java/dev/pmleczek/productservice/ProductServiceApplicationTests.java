a32f6ac0b3d923afcbab8b3ab91c636a00e1e7ae
hyper <patrykmleczek.pm@gmail.com>
2023-04-05 18:20:58 +0200

    Initialize product-service project with Spring Initializr

---
+++ product-service/src/test/java/dev/pmleczek/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pmleczek.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
