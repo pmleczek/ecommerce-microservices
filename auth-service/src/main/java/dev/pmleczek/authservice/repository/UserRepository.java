f3428e79928f4820cec501961f2e061c3ad4f03b
hyper <patrykmleczek.pm@gmail.com>
2023-04-05 20:09:40 +0200

    Add UserRepository repository to auth-service

---
+++ auth-service/src/main/java/dev/pmleczek/authservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dev.pmleczek.authservice.repository;
+
+import dev.pmleczek.authservice.entity.User;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+    Mono<User> findUserByEmail(String email);
+}
