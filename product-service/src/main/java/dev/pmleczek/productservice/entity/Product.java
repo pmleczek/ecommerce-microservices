fa08523cb94e92a4674ea77025cb4e96c9749816
hyper <patrykmleczek.pm@gmail.com>
2023-04-05 18:30:02 +0200

    Add Product entity to product-service

---
+++ product-service/src/main/java/dev/pmleczek/productservice/entity/Product.java
@@ -0,0 +1,133 @@
+package dev.pmleczek.productservice.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection = "products")
+public class Product {
+    @Id
+    private String id;
+    @Version
+    private Long version;
+    private String name;
+    private String description;
+    private Double price;
+    @CreatedDate
+    private Date created;
+    @LastModifiedDate
+    private Date lastModified;
+
+    public Product() {
+    }
+
+    public Product(Long version, String name, String description, Double price, Date created, Date lastModified) {
+        this.version = version;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.created = created;
+        this.lastModified = lastModified;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Date lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", version=" + version +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", created=" + created +
+                ", lastModified=" + lastModified +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (!id.equals(product.id)) return false;
+        if (!version.equals(product.version)) return false;
+        if (!name.equals(product.name)) return false;
+        if (!description.equals(product.description)) return false;
+        if (!price.equals(product.price)) return false;
+        if (!created.equals(product.created)) return false;
+        return lastModified.equals(product.lastModified);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + version.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + description.hashCode();
+        result = 31 * result + price.hashCode();
+        result = 31 * result + created.hashCode();
+        result = 31 * result + lastModified.hashCode();
+        return result;
+    }
+}
