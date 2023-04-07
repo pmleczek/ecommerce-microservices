e7c9a5ca3e4d085fd0db012c44a0927e75dd47d9
hyper <patrykmleczek.pm@gmail.com>
2023-04-07 11:27:54 +0200

    Fix typo in ProductRepository

--- product-service/src/main/java/dev/pmleczek/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/dev/pmleczek/productservice/repository/ProductRepository.java
@@ -3,5 +3,5 @@ package dev.pmleczek.productservice.repository;
 import dev.pmleczek.productservice.entity.Product;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface ProductRespository extends ReactiveMongoRepository<Product, String> {
+public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
 }
