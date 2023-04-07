d75c335572114d5e0acb064b79bff0656c6751a0
hyper <patrykmleczek.pm@gmail.com>
2023-04-07 21:39:10 +0200

    Add product-service component to Helm charts

--- devk8s.sh
+++ devk8s.sh
@@ -10,3 +10,23 @@ gradle clean build -x test
 docker build -t product-service .
 minikube image load product-service
 cd ..
+
+cd kubernetes/helm
+cd components
+
+cd auth-service
+helm dependency update
+helm dependency build
+cd ..
+
+cd product-service
+helm dependency update
+
+helm dependency build
+cd ..
+
+cd ..
+cd environments
+helm dependency update development
+helm dependency build development
+helm install development development
